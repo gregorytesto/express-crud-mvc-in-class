1bd95911b2c166ad734063cf600bddb84bf257ca
Greg Testo <gregt@pursuit.org>
2022-01-12 12:54:25 -0400

    added put and delete routes

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
+const bookmarksController = require("./controllers/bookmarksController.js");
 
 app.use(express.json());
-
-const bookmarksController = require("./controllers/bookmarksController.js");
 
 app.get("/", (req, res)=>{
     res.send("Welcome to our bookmarks app");
 });
 
 app.use("/bookmarks", bookmarksController);
-
 
 app.get("*", (req, res)=>{
     res.status(404).json({ error: "Page not found" });
--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -13,14 +13,47 @@ bookmarkRoutes.get("/:index", (req, res)=>{
     if(bookmarksArr[index]){
         res.json(bookmarksArr[index]);
     } else {
-        res.status(404).json({message: "Bookmark not found"});
+        res.status(404).json({error: "Not found"});
     }
 });
-
+  
 // /bookmarks
 bookmarkRoutes.post("/", (req, res)=>{
     bookmarksArr.push(req.body);
     res.json(bookmarksArr[bookmarksArr.length-1]);
+})
+
+bookmarkRoutes.delete("/:index", (req, res)=>{
+    const { index } = req.params;
+    if(bookmarksArr[index]){
+        let removed = bookmarksArr.splice(index, 1);
+        res.json(removed[0]);
+    } else {
+        res.status(404).json({error: "Not found"});
+    }
+})
+
+bookmarkRoutes.put("/:index", (req, res)=>{
+    let { index } = req.params;
+
+    if(!bookmarksArr[index]){
+        res.status(422).json({
+            error: "Not found"
+        })
+        return;
+    }
+
+    let { name, url, isFavorite, category } = req.body;
+    if(name && url && isFavorite !== undefined && category){
+        bookmarksArr[index] = {
+            name, url, isFavorite, category
+        };
+        res.json(bookmarksArr[index]);
+    } else {
+        res.status(422).json({
+            error: "Please provide all fields"
+        })
+    }
 })
 
 module.exports = bookmarkRoutes;
